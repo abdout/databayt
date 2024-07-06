c0840b6c90b39521dc31c3bcda4aeb94a4f18bcb
abdout <osmanabdout@hotmail.com>
2024-07-06 14:51:43 +0300

    x

--- src/auth.ts
+++ src/auth.ts
@@ -35,7 +35,7 @@ export const {
       const existingUser = await getUserById(user.id);
 
       // Prevent sign in without email verification
-      if (!existingUser?.emailVerified) return false;
+      // if (!existingUser?.emailVerified) return false;
 
       if (existingUser && existingUser.isTwoFactorEnabled) {
         const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(existingUser.id);
