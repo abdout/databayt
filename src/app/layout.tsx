c9f5cf052240df7e7976b5f401a67e7301797b30
abdout <osmanabdout@hotmail.com>
2024-10-03 18:44:34 +0300

    x

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,8 @@ import { Toaster } from "@/components/ui/sonner";
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Auth',
-  description: 'Authentication block',
+  title: 'Databayt',
+  description: 'Bussiness automation',
 }
 
 export default async function RootLayout({
