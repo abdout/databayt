5bcf8fb3a351a97b84bedaf5787515e56e6e2acd
abdout <osmanabdout@hotmail.com>
2024-11-18 13:24:39 +0300

    x

--- src/components/landing/clock.tsx
+++ src/components/landing/clock.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState } from 'react';
 
 const Clock = () => {
     const [isReverse, setIsReverse] = useState(false);
