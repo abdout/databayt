3268980b21a5a4e64e005ed06362c910c8c73426
Databayt <osmanabdout@hotmail.com>
2024-12-12 02:27:57 +0300

    x

--- src/hooks/use-config.ts
+++ src/hooks/use-config.ts
@@ -1,33 +1,25 @@
-"use client"
-import { useAtom } from "jotai"
-import { atomWithStorage } from "jotai/utils"
-import { BaseColor } from "@/components/wizard/base-color"
+"use client";
+import { useAtom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
+import { BaseColor } from "@/components/wizard/base-color";
 
-const styles = [
-  {
-    name: "new-york",
-    label: "New York",
-  },
-  {
-    name: "default",
-    label: "Default",
-  },
-] as const // Use `as const` to derive literal types
-
-type Style = (typeof styles)[number]
+type Style = {
+  name: "new-york" | "default";
+  label: string;
+};
 
 type Config = {
-  style: Style["name"]
-  theme: BaseColor["name"]
-  radius: number
-}
+  style: Style["name"];
+  theme: BaseColor["name"];
+  radius: number;
+};
 
 const configAtom = atomWithStorage<Config>("config", {
   style: "default",
   theme: "zinc",
   radius: 0.5,
-})
+});
 
 export function useConfig() {
-  return useAtom(configAtom)
+  return useAtom(configAtom);
 }
