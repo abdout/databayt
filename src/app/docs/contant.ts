61e09bf7b1f542313e248f861d7e1699e5120dbb
abdout <osmanabdout@hotmail.com>
2025-06-11 07:31:05 +0300

    n

---
+++ src/app/docs/contant.ts
@@ -0,0 +1,50 @@
+export const appsData = [
+  {
+    name: "Databayt",
+    description: "Marketing page",
+    repositoryUrl: "https://github.com/abdout/databayt",
+    previewUrl: "https://databayt.org"
+  },
+  {
+    name: "Codebase",
+    description: "Code library",
+    repositoryUrl: "https://github.com/abdout/codebase",
+    previewUrl: "https://cb.databayt.org"
+  },
+  {
+    name: "Company",
+    description: "Company automation",
+    repositoryUrl: "https://github.com/abdout/company",
+    previewUrl: "https://co.databayt.org"
+  },
+  {
+    name: "School",
+    description: "School automation",
+    repositoryUrl: "https://github.com/abdout/hogwarts",
+    previewUrl: "https://ed.databayt.org"
+  },
+  {
+    name: "Health",
+    description: "Healthcare automation",
+    repositoryUrl: "https://github.com/abdout/shifa",
+    previewUrl: "https://hc.databayt.org"
+  },
+  {
+    name: "Real Estate",
+    description: "Property automation",
+    repositoryUrl: "https://github.com/abdout/mkan",
+    previewUrl: "https://mk.databayt.org"
+  },
+  {
+    name: "Public Party",
+    description: "Party automation",
+    repositoryUrl: "https://github.com/abdout/nmbd",
+    previewUrl: "https://nmbdsd.org"
+  },
+  {
+    name: "Food",
+    description: "Food automation",
+    repositoryUrl: "https://github.com/abdout/food",
+    previewUrl: "https://fd.databayt.org"
+  }
+]
